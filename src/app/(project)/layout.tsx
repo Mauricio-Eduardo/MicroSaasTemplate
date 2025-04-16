e4465531f3fac940c36673fceaa4b58e9bd58586
MAURICIO <mauricio11.ita@hotmail.com>
2025-04-16 20:42:53 -0300

    fix build errors

--- src/app/(project)/layout.tsx
+++ src/app/(project)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter, Poppins } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
