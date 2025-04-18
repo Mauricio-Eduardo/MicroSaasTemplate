aa20a954fc360d41843fca935796ca92151251b9
MAURICIO <mauricio11.ita@hotmail.com>
2025-04-18 19:51:18 -0300

    icons

--- src/app/(project)/page.tsx
+++ src/app/(project)/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-   <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Landing Page</h1>
-   </div>
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
+    <h1 className="text-4xl font-bold mb-10">Landing Page</h1>
+  
+    <Link href="/login" className="border rounded-md px-2 py-1 cursor-pointer">
+        Login
+    </Link>
+</div>
   );
 }
