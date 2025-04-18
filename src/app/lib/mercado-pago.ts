53c9921b4e4e9128195c2a09811a3edfcf18f8f5
MAURICIO <mauricio11.ita@hotmail.com>
2025-04-17 21:58:54 -0300

    fix mercado-pago-webhook

--- src/app/lib/mercado-pago.ts
+++ src/app/lib/mercado-pago.ts
@@ -6,57 +6,53 @@ export const mpClient = new MercadoPagoConfig({
     accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN!,
 });
 
-export function validateMercadoPagoWebhook(request: NextRequest) {
-    const xSignature = request.headers.get("x-signature");
-    const xRequestId = request.headers.get("x-request-id");
-
-    if (!xSignature || xRequestId) {
-        return NextResponse.json(
-            { error: "Missing x-signature or x-request-id header"}, 
-            { status: 400 }
-        );   
+export function validateMercadoPagoWebhook(req: NextRequest) {
+    const xSignature = req.headers.get("x-signature");
+    const xRequestId = req.headers.get("x-request-id");
+    if (!xSignature || !xRequestId) {
+      return NextResponse.json(
+        { error: "Missing x-signature or x-request-id header" },
+        { status: 400 }
+      );
     }
-
+  
     const signatureParts = xSignature.split(",");
-    let ts = ""
-    let v1 = ""
+    let ts = "";
+    let v1 = "";
     signatureParts.forEach((part) => {
-        const [key, value] = part.split("=");
-        if (key.trim() === "ts") {
-            ts = value.trim();
-        } else if (key.trim() === "v1") {
-            v1 = value.trim();
-        }
-    })
-
+      const [key, value] = part.split("=");
+      if (key.trim() === "ts") {
+        ts = value.trim();
+      } else if (key.trim() === "v1") {
+        v1 = value.trim();
+      }
+    });
+  
     if (!ts || !v1) {
-        return NextResponse.json(
-            { error: "Invalid x-signature header format" }, 
-            { status: 400 }
-        );
+      return NextResponse.json(
+        { error: "Invalid x-signature header format" },
+        { status: 400 }
+      );
     }
-
-    const url = new URL(request.url);
+  
+    const url = new URL(req.url);
     const dataId = url.searchParams.get("data.id");
-
-    let manifest = ""
+  
+    let manifest = "";
     if (dataId) {
-        manifest += `id:"${dataId};`;
-    } 
+      manifest += `id:${dataId};`;
+    }
     if (xRequestId) {
-        manifest += `request-id:${xRequestId};`;
+      manifest += `request-id:${xRequestId};`;
     }
     manifest += `ts:${ts};`;
-
+  
     const secret = process.env.MERCADO_PAGO_WEBHOOK_SECRET!;
     const hmac = crypto.createHmac("sha256", secret);
     hmac.update(manifest);
     const generatedHash = hmac.digest("hex");
-
+  
     if (generatedHash !== v1) {
-        return NextResponse.json(
-            { error: "Invalid signature" }, 
-            { status: 401 }
-        );
+      return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
     }
-}
+  }
